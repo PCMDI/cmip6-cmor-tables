c10274a169c5716da576b0bb2a59e1792c5083b1
mo-kerstinschmatzer <kschmatz@vld733.cmpd1.metoffice.gov.uk>
2023-01-06 14:57:08 +0000

    CDDSO-230 Update plugin interface
    * Add get stream_info method
    * Add streams_config.json
    * Add model_file_info

--- cdds_arise/arise_plugin.py
+++ cdds_arise/arise_plugin.py
@@ -11,24 +11,26 @@ import logging
 import os
 from typing import Type
 
-from cdds_common.cdds_plugins.grid import GridLabel
-from cdds_common.cdds_plugins.models import ModelParameters
-from cdds_common.cdds_plugins.plugins import CddsPlugin
-from cdds_common.cdds_plugins.common import LoadResults
+from cdds.common.plugins.file_info import ModelFileInfo, GlobalModelFileInfo
+from cdds.common.plugins.grid import GridLabel
+from cdds.common.plugins.models import ModelParameters
+from cdds.common.plugins.plugins import CddsPlugin
+from cdds.common.plugins.common import LoadResults
 
-from cdds_common.cdds_plugins.base.base_models import BaseModelStore
+from cdds.common.plugins.base.base_models import BaseModelStore
+from cdds.common.plugins.base.base_streams import BaseStreamInfo, BaseStreamStore
 
-from cdds_common.cdds_plugins.cmip6.cmip6_models import (
+from cdds.common.plugins.cmip6.cmip6_models import (
     UKESM1_0_LL_Params, HadGEM3_GC31_LL_Params)
-from cdds_common.cdds_plugins.cmip6.cmip6_grid import Cmip6GridLabel
-from cdds_common.cdds_plugins.cmip6.cmip6_models import Cmip6ModelsStore
-import cdds_common.cdds_plugins.cmip6 as cmip6
+from cdds.common.plugins.cmip6.cmip6_grid import Cmip6GridLabel
+import cdds.common.plugins.cmip6 as cmip6
+from cdds.common.plugins.streams import StreamInfo
 
 
 class ArisePlugin(CddsPlugin):
 
     def __init__(self):
-        super(ArisePlugin, self).__init__("arise")
+        super(ArisePlugin, self).__init__("ARISE")
 
     def models_parameters(self, model_id: str) -> ModelParameters:
         models_store = AriseModelStore.instance()
@@ -41,6 +43,13 @@ class ArisePlugin(CddsPlugin):
     def grid_labels(self) -> Type[GridLabel]:
         # Use CMIP6 settings for grid labels
         return Cmip6GridLabel
+
+    def stream_info(self) -> StreamInfo:
+        stream_store = AriseStreamStore.instance()
+        return stream_store.get()
+
+    def model_file_info(self) -> ModelFileInfo:
+        return GlobalModelFileInfo()
 
 
 class AriseModelStore(BaseModelStore):
@@ -76,3 +85,22 @@ class AriseModelStore(BaseModelStore):
             ]
             self.logger.critical('\n'.join(error_messages))
             raise RuntimeError('\n'.join(error_messages))
+
+
+class AriseStreamInfo(BaseStreamInfo):
+
+    def __init__(self, config_path: str = '') -> None:
+        if not config_path:
+            local_dir = os.path.dirname(os.path.abspath(__file__))
+            config_path = os.path.join(local_dir, 'data/streams/streams_config.json')
+        super(AriseStreamInfo, self).__init__(config_path)
+
+
+class AriseStreamStore(BaseStreamStore):
+    def __init__(self) -> None:
+        stream_info = AriseStreamInfo()
+        super(AriseStreamStore, self).__init__(stream_info)
+
+    @classmethod
+    def create_instance(cls) -> 'AriseStreamStore':
+        return AriseStreamStore()
